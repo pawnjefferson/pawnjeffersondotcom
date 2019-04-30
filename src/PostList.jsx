a2bed0ba4a431af778de9987c3707def17e52bde
pawnjefferson <pawnjefferson@gmail.com>
2019-04-29 23:11:15 -0400

    GIT IT

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -21,6 +21,11 @@ let PostList = [
     date :`4/28/2019 9:31PM`,
     title: `Yesterday I drove from Destin, Florida to St. Petersburg, Florida.  Phew that was another longgg drive.  Also said what's up on my way down to my friends in Hernando Beach at their place right on the Gulf of Mexico.  Fun times playing Chess and trying to get them to finally take me time traveling with them.  Today I recorded grand piano and some authentic wurlitzer electric piano tracks for my latest three songs.  Can't wait to share them.  They sound fire.  Speaking of flame I got some fireeee.  Let's go LEAS this week.  Stay tuned for more quality content!!!`,
     html: `<img src="img/pawnthread.jpg">`
+  },
+  {
+    date: `4/29/2019 11:10PM`,
+    title: 'MORE PAWN BALLERSON CHATTER ON THE IHUB BOARD!!! WOAH WAHHH WEE WAH WAHHHHH',
+    html: `<div class="videoWrapper"><iframe src="https://www.youtube.com/embed/mc12O4lrzl8" frameborder="0" allowfullscreen></iframe></div>`
   }
 ];
 
